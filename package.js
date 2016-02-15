bf3b1fbe286a6e9be337a15fe60cb508a314de3a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-02-15 17:22:20 -0200

    update

--- package.js
+++ package.js
@@ -1,22 +1,18 @@
 Package.describe({
-  name: 'typ:ldapjs',
-  version: '0.7.3',
+  name: 'rocketchat:ldapjs',
+  version: '1.0.0',
   summary: 'Meteor package wrapper for the ldapjs Npm module https://www.npmjs.com/package/ldapjs',
-  git: 'https://github.com/typ90/meteor-ldapjs',
+  git: 'https://github.com/RocketChat/meteor-ldapjs',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  ldapjs: "0.7.1", 
+  ldapjs: "1.0.0",
 });
 
 Package.onUse(function (api) {
 
-  api.add_files([
-    'lib/ldapjs.js',
-  ], ['server']);
-  
-  api.export([
-    'MeteorWrapperLdapjs'
-  ]);
+  api.add_files('lib/ldapjs.js', 'server');
+
+  api.export('LDAPJS');
 });
